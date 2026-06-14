a2b776a7f1a4de648e98a1d9adcce2c16beea9f4
nourelhoudaas <sayahhouda06@gmail.com>
2026-06-14 18:31:55 +0100

    fix: add new vercel url to cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://mern-taskflow.vercel.app'
+    'https://mern-taskflow-seven.vercel.app'
   ],
   credentials: true
 }))
